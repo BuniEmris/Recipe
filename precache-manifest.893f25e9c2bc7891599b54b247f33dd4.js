9deb687d45e725423dfe2d430227fb1f59383266
Buni Emris <bsaydaliev@inbox.ru>
2020-09-05 13:13:16 +0500

    Updates

---
+++ precache-manifest.893f25e9c2bc7891599b54b247f33dd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e6c336675ef17a9f715c6054bb3874",
+    "url": "/BuniEmris/Recipe.git/index.html"
+  },
+  {
+    "revision": "94f86139ace4540f23b7",
+    "url": "/BuniEmris/Recipe.git/static/css/main.5c5f53da.chunk.css"
+  },
+  {
+    "revision": "4409f4c215457acdbfa5",
+    "url": "/BuniEmris/Recipe.git/static/js/2.33e35ca9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BuniEmris/Recipe.git/static/js/2.33e35ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f86139ace4540f23b7",
+    "url": "/BuniEmris/Recipe.git/static/js/main.8ba071ef.chunk.js"
+  },
+  {
+    "revision": "db42b3cb5ac6496b0da5",
+    "url": "/BuniEmris/Recipe.git/static/js/runtime-main.0c280281.js"
+  },
+  {
+    "revision": "c0a8671b0cb8c30736aaf31d36f1355f",
+    "url": "/BuniEmris/Recipe.git/static/media/logo1.c0a8671b.png"
+  }
+]);
